5a293b7dedd8859b5185c336c3dd5db91a9f8597
lordafzal <32799810+lordafzal@users.noreply.github.com>
2017-12-11 00:54:57 -0500

    Add files via upload

---
+++ setup/deleter.sql
@@ -0,0 +1,21 @@
+-- Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+--
+-- You may not modify, use, reproduce, or distribute this software
+-- except in compliance with the terms of the license at:
+-- http://developer.sun.com/berkeley_license.html
+--
+-- author: tgiunipero
+--
+
+--
+-- Database: `affablebean`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Sample data for table `category`
+--
+
+DELETE FROM product;
+DELETE FROM category;
